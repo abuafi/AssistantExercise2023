4d4a90468a5f8f68520e4e12fb8fb202f50aa6a1
Ian Abuaf <abuafi@usi.ch>
2022-12-23 12:35:59 +0100

    Added ThreadPool and handled some possible errors

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 from urllib.parse import urlparse
+from threading import Thread
+from queue import Queue
+from warnings import warn
+
+from concurrent.futures import ThreadPoolExecutor
 
 from parsers.CourseParserUSI import CourseParserUSI
 
@@ -8,10 +13,24 @@ parse_switch = {
 
 def parse_url(url:str) -> None:
     domain = urlparse(url).netloc
-    parser = parse_switch[domain](url)
-    return parser.parse_stats()
+    try:
+        parser = parse_switch[domain](url)
+        return parser.parse_stats()
+    except KeyError:
+        warn(f"Domain name {domain} not supported!")
 
 if __name__ == "__main__":
-    url = "https://search.usi.ch/en/courses/35265726/advanced-java-programming"
-    data = parse_url(url)
-    print(data)
+    urls = [
+    "https://search.usi.ch/en/courses/35265726/advanced-java-programming",
+    "https://studentservices.uzh.ch/uzh/anonym/vvz/index.html?sap-language=EN&sap-ui-language=EN#/details/2022/004/SM/51048814/50000003/Wirtschaftswissenschaftliche%2520Fakult%25C3%25A4t/50773260/Master%2520of%2520Science%2520UZH%2520in%2520Informatik%2520(RVO16)/50774406/Computing%2520and%2520Economics",
+    "https://search.usi.ch/en/courses/35265666/programming-styles",
+    "https://search.usi.ch/en/courses/352653/",
+    "https://search.usi.ch/en/courses/35265754/distributed-algorithms",
+    ]
+
+    dataqueue = Queue()
+
+    with ThreadPoolExecutor() as executor:
+        results = list(executor.map(parse_url, urls))
+        results = [x for x in results if x is not None]
+        print(results)
--- src/parsers/CourseParser.py
+++ src/parsers/CourseParser.py
@@ -7,14 +7,26 @@ class CourseParser(ABC):
     def __init__(self, url) -> None:
         self.url = url
 
+    def warn(self, string):
+        warn(f"{string} in parser {str(self.__class__)} for url {self.url}")
+
     def parse_stats(self):
         req = requests.get(self.url, timeout=3)
+        if req.status_code != 200:
+            self.warn(f"Unexpected status code {req.status_code}")
+            return
+
         html = req.text
         soup = BeautifulSoup(html, "html.parser")
-        return {
-        'Teaching Staff':self.parse_staff(soup),
-        'Description':self.parse_description(soup).strip()
-        }
+        try:
+            staff = self.parse_staff(soup)
+            description = self.parse_description(soup).strip()
+            return {
+            'Teaching Staff':staff,
+            'Description':description
+            }
+        except Exception:
+            self.warn(f"Exception in parser")
 
     @abstractmethod
     def parse_staff(self, soup:BeautifulSoup): pass
